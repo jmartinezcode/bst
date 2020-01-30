a4cb0a5a201f0f9915a631ac59cf685e8a340555
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-30 15:47:37 -0600

    Added Add and Search

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -8,5 +8,63 @@ namespace BinarySearchTree
 {
     class BinarySearchTree
     {
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public Node Search(int data, Node node) // return bool? possibly whether exists
+        {
+            if (node == null || node.data == data)
+            {
+                return node;
+            }
+            if (data < node.data )
+            {
+                return Search(data, node.leftNode);
+            }
+            else
+            {
+                return Search(data, node.rightNode);
+            }
+        }
+
+        public void Add(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (data < current.data)
+                    {
+                        current = current.leftNode;
+                        if (current == null)
+                        {
+                            parent.leftNode = node;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.rightNode;
+                        if (current == null)
+                        {
+                            parent.rightNode = node;
+                            break;
+                        }
+                    }
+                }            
+            }
+        }
     }
 }
